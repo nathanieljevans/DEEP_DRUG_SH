540d719c48389a80359d589fe00d6a9f7af9eabb
nathanieljevans <Nathaniel.Evansj@gmail.com>
2019-11-25 23:25:50 -0800

    what did I not change...

---
+++ python/pytorch_data_separation.py
@@ -0,0 +1,8 @@
+'''
+This is the last step to generating the data we will use for the deep learning project.
+
+This script:
+  1. separates expression data into individual files
+    a.
+  2. transforms gene targets into vector encoding
+'''
