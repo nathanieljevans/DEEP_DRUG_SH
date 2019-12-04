d0ade46324074042bcc6db16c2525a032ea3948f
Nathaniel Evans <nathaniel.evansj@gmail.com>
2019-12-03 20:33:16 -0800

    adding expr groupby for dataset comparison

--- python/dataset_response_comparison.py
+++ python/dataset_response_comparison.py
@@ -20,34 +20,45 @@ if __name__ == '__main__':
     with open('./../data_pytorch/split_label_dict.pkl', 'rb') as f:
         label_dict = pickle.load(f)
 
-    targets = {'dataset' : [], 'target' : [], 'response' : []}
+    targets = {'dataset' : [], 'target' : [], 'response' : [], 'expr' : []}
     for i,dataset in enumerate([dataset for dataset in params['RESP_TYPES']]):
         test = label_dict[dataset]['test']
         gen = data.DataLoader(utils.DrugExpressionDataset(test, root_dir='./../data_pytorch/tensors/', return_response_type=True), **{'batch_size':1, 'shuffle':False,'num_workers':0})
         ii = 0
         for X,y,resp_type,resp_selector in gen:
             ii+=X.size(0)
-            #if ii > 1000:
-            #    break
+            if ii > 1000:
+                break
             if ii/X.size(0) % 1000 == 0:
                 print(f'predicting {dataset} ...[{ii}/{len(gen.dataset)}]', end='\r')
             targets['dataset'].append(dataset)
             targets['target'].append(hash(str(X.numpy()[:,1])))
             targets['response'].append(y.numpy()[0])
+            targets['expr'].append(hash(str(np.round(X.numpy()[:,0]))))
+
         print()
 
     df = pd.DataFrame(targets)
-    df = df.groupby(['dataset', 'target']).agg({'response':'mean'})
+    df = df.groupby(['dataset', 'target', 'expr']).agg({'response':'mean'})
     df = df.reset_index()
+
+    print(df.head())
 
     f, axes = plt.subplots(5,3,figsize=(15,10))
     corr = {'dataset1':[],'dataset2':[],'corr':[]}
     ii = 0
     for i, dataset1 in enumerate([dataset for dataset in params['RESP_TYPES']]):
         for j,dataset2 in enumerate([dataset for dataset in params['RESP_TYPES']][(i+1):]):
+            print(f'{dataset1} | {dataset2}')
             tmp = df[df['dataset'].isin([dataset1, dataset2])]
             intersection_targets = np.intersect1d(tmp[tmp['dataset'] == dataset1].target.values, tmp[tmp['dataset'] == dataset2].target.values)
-            tmp = tmp[tmp['target'].isin(intersection_targets)].pivot(index='target', columns='dataset', values='response')
+            intersection_expr = np.intersect1d(tmp[tmp['dataset'] == dataset1].expr.values, tmp[tmp['dataset'] == dataset2].expr.values)
+            print(intersection_expr)
+            tmp = tmp[tmp['target'].isin(intersection_targets)]
+            print(tmp.head())
+            tmp = tmp[tmp['expr'].isin(intersection_expr)]
+            print(tmp.head())
+            tmp = tmp.pivot(index=['target','expr'], columns='dataset', values='response')
             print(f'[{dataset1}, {dataset2}] intersecting targets: {tmp.values.shape[0]}')
             if tmp.values.shape[0] > 10:
                 corr['dataset1'].append(dataset1)
