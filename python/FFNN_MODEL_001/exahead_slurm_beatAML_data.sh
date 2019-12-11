fd56aef86901b15dc4abc1b15901843c34ff3ec2
evansna <nathaniel.evansj@gmail.com>
2019-12-10 16:41:54 -0800

    training on exacloud; using to veiw plots

--- python/FFNN_MODEL_001/exahead_slurm_beatAML_data.sh
+++ python/FFNN_MODEL_001/exahead_slurm_beatAML_data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#SBATCH --job-name=evans_cpu_beatAML_train
+#SBATCH --job-name=aml_01
 #SBATCH --time=06:00:00
 #SBATCH --mem=12G
 #SBATCH -c 12
-#SBATCH --out=./../../data_pytorch/logfile/cpu_beataml_train_LOG.txt
-#SBATCH --error=./../../data_pytorch/logfile/cpu_beataml_train_ERROR.txt
+#SBATCH --out=./../../data_pytorch/logfile/cpu_beataml_train_LOG001.txt
+#SBATCH --error=./../../data_pytorch/logfile/cpu_beataml_train_ERROR001.txt
 
 python ~/DEEP_DRUG_SH/python/FFNN_MODEL_001/train.py --config config--beatAML_data
 
